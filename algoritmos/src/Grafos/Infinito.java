2c085d6daac5f7fc18027f748281665ef87777a4
unknown <federico2002castro@gmail.com>
2024-06-24 22:38:17 -0300

    Matriz para grafos

---
+++ algoritmos/src/Grafos/Infinito.java
@@ -0,0 +1,5 @@
+package Grafos;
+
+public class Infinito {
+	public static final int INFINITO = Integer.MAX_VALUE;
+}
--- algoritmos/src/Main.java
+++ algoritmos/src/Main.java
@@ -1,12 +1,20 @@
 import DivisionYConquista.Sumar;
+import Grafos.Infinito;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[] elementos = {1, 2, 3, 4, 5};
+        /*int[] elementos = {1, 2, 3, 4, 5};
 
         Sumar sumar = new Sumar();
-        sumar.calcular(elementos);
+        sumar.calcular(elementos);*/
+    	
+    	// Main para grafos
+    int[][] grafo_ponderado = {{Grafos.Infinito.INFINITO, 3, 5, Grafos.Infinito.INFINITO,
+    							Grafos.Infinito.INFINITO, Grafos.Infinito.INFINITO, 1, 4,
+    							9, 5, 7, Grafos.Infinito.INFINITO,
+    							3, Grafos.Infinito.INFINITO, Grafos.Infinito.INFINITO, Grafos.Infinito.INFINITO}};
+    	
     }
 }
 
